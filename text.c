dea2377cb181a3efa6632afdfa04cb112df510da
tad59 <rusakt@poczta.onet.pl>
2025-03-14 20:46:18 +0100

    nowy program pytanie o imie

--- text.c
+++ text.c
@@ -1,1 +1,12 @@
-test
+#include <stdio.h>
+
+int main() {
+    char name[100];
+
+    printf("Enter your name: ");
+    fgets(name, sizeof(name), stdin);
+
+    printf("Hello, %s", name);
+
+    return 0;
+}
